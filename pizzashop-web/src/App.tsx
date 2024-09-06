722f2b76f1c0a0b78c3c18afc8e012376cf66427
nikolasraniery <nikolassraniery@gmail.com>
2024-09-06 13:50:41 -0300

    [PS-02] - feat: Implementando telas de dashboard e pedidos

--- pizzashop-web/src/App.tsx
+++ pizzashop-web/src/App.tsx
@@ -2,14 +2,19 @@ import './index.css'
 
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
+import { Toaster } from 'sonner'
 
+import { ThemeProvider } from '@/components/theme/theme-provider.tsx'
 import { router } from '@/routes.tsx'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | Pizza Shop" />
-      <RouterProvider router={router} />
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
+        <Helmet titleTemplate="🍕 %s | Pizza Shop" />
+        <Toaster position="top-right" richColors />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
--- pizzashop-web/src/routes.tsx
+++ pizzashop-web/src/routes.tsx
@@ -3,17 +3,25 @@ import { createBrowserRouter } from 'react-router-dom'
 import { AppLayout } from '@/pages/_layouts/app.tsx'
 import { AuthLayout } from '@/pages/_layouts/auth.tsx'
 import { Dashboard } from '@/pages/app/dashboard.tsx'
+import { Orders } from '@/pages/app/orders/orders.tsx'
 import { SignIn } from '@/pages/auth/sign-in.tsx'
+import { SignUp } from '@/pages/auth/sign-up.tsx'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
-    children: [{ path: '/', element: <Dashboard /> }],
+    children: [
+      { path: '/', element: <Dashboard /> },
+      { path: '/orders', element: <Orders /> },
+    ],
   },
   {
     path: '/',
     element: <AuthLayout />,
-    children: [{ path: '/sign-in', element: <SignIn /> }],
+    children: [
+      { path: '/sign-in', element: <SignIn /> },
+      { path: '/sign-up', element: <SignUp /> },
+    ],
   },
 ])
