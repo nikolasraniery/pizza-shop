4668b74b6941cc9c834781da1c104885d0acc2cb
nikolasraniery <nikolassraniery@gmail.com>
2024-09-23 13:32:41 -0300

    [PS-05] - feat: Adicionando gráficos à tela de dashboard

--- pizzashop-web/src/pages/app/dashboard/dashboard.tsx
+++ pizzashop-web/src/pages/app/dashboard/dashboard.tsx
@@ -4,6 +4,8 @@ import { DayOrdersAmountCard } from '@/pages/app/dashboard/day-orders-amount-car
 import { MonthCanceledOrdersAmountCard } from '@/pages/app/dashboard/month-canceled-orders-amount-card.tsx'
 import { MonthOrdersAmountCard } from '@/pages/app/dashboard/month-orders-amount-card.tsx'
 import { MonthRevenueCard } from '@/pages/app/dashboard/month-revenue-card.tsx'
+import { PopularProductsChart } from '@/pages/app/dashboard/popular-products-chart.tsx'
+import { RevenueChart } from '@/pages/app/dashboard/revenue-chart.tsx'
 
 export function Dashboard() {
   return (
@@ -17,6 +19,11 @@ export function Dashboard() {
           <MonthOrdersAmountCard />
           <DayOrdersAmountCard />
           <MonthCanceledOrdersAmountCard />
+        </div>
+
+        <div className="grid grid-cols-9 gap-4">
+          <RevenueChart />
+          <PopularProductsChart />
         </div>
       </div>
     </>
