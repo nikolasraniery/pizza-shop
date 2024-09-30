cc8043432978d96e605e2dc7508fc97311485a78
nikolasraniery <nikolassraniery@gmail.com>
2024-09-30 11:09:22 -0300

    [PS-10] - feat: Adicionando dados dinâmicos vindos do backend

--- pizzashop-web/src/components/account-menu.tsx
+++ pizzashop-web/src/components/account-menu.tsx
@@ -1,5 +1,8 @@
+import { useQuery } from '@tanstack/react-query'
 import { Building, ChevronDown, LogOut } from 'lucide-react'
 
+import { getManagedRestaurant } from '@/api/get-managed-restaurant.ts'
+import { getProfile } from '@/api/get-profile.ts'
 import { Button } from '@/components/ui/button.tsx'
 import {
   DropdownMenu,
@@ -11,6 +14,17 @@ import {
 } from '@/components/ui/dropdown-menu.tsx'
 
 export function AccountMenu() {
+  const { data: profile, isLoading: isLoadingProfile } = useQuery({
+    queryKey: ['profile'],
+    queryFn: getProfile,
+  })
+
+  const { data: managedRestaurant, isLoading: isLoadingManagedRestaurant } =
+    useQuery({
+      queryKey: ['managed-restaurant'],
+      queryFn: getManagedRestaurant,
+    })
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -18,15 +32,15 @@ export function AccountMenu() {
           variant="outline"
           className="flex select-none items-center gap-2"
         >
-          Pizza Shop
+          {managedRestaurant?.name}
           <ChevronDown className="h-4 w-4" />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="w-56">
         <DropdownMenuLabel className="flex flex-col">
-          <span>Nikolas Santos</span>
+          <span>{profile?.name}</span>
           <span className="text-sm font-normal text-muted-foreground">
-            nikolassraniery@gmail.com
+            {profile?.email}
           </span>
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
